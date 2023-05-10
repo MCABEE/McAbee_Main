87d8949272ee77e5755af16f346033197bb26168
Iniy96 <iniyavansivaraj07@gmail.com>
2023-05-10 19:52:18 +0530

    completed header design

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,19 +1,19 @@
 import React, { useState } from 'react'
 import "./navbar.css"
 import { RiMenu3Fill } from "react-icons/ri"
-import { FaLinkedinIn,FaFacebookF,FaTwitter } from "react-icons/fa"
+import { FaLinkedinIn, FaFacebookF, FaTwitter } from "react-icons/fa"
 import { Link } from 'react-router-dom'
 
 export const Navbar = () => {
   return (
     <>
-      <div className=' container mx-auto w-100'>
-        <div className='d-flex justify-content-between align-items-center pt-3 container position-absolute '>
+      <div className='container navigation'>
+        <div className=' container d-flex position-relative align-items-center justify-content-between pt-3 '>
           <div className='fs-3 text-white'>
-            MCABEE
+          <Link to="/" className='text-white'>MCABEE</Link>
           </div>
-          <div>
-            <input type="checkbox" id="check" className='z-3' />
+          <div className=''>
+            <input type="checkbox" id="check" />
             <label for="check">
               <RiMenu3Fill color={"#8dcff4"} size={25} />
             </label>
@@ -30,18 +30,15 @@ export const Navbar = () => {
                   <li><Link className="nav-link navigation-link" to="/contact">Contact</Link></li>
                 </ul>
                 <div className='text-center pb-4'>
-                  <a href="https://www.linkedin.com/"><FaLinkedinIn className='iconliks ' size={26} color='#0579b6'/></a>
-                  <a href="https://www.facebook.com/"><FaFacebookF className='mx-4 iconliks ' size={26} color='#4b69b1'/></a>
-                  <a href="https://www.twitter.com/"><FaTwitter className='iconliks' size={26} color='#37b1e2'/></a>
+                  <a href="https://www.linkedin.com/"><FaLinkedinIn className='iconliks ' size={26} color='#0579b6' /></a>
+                  <a href="https://www.facebook.com/"><FaFacebookF className='mx-4 iconliks ' size={26} color='#4b69b1' /></a>
+                  <a href="https://www.twitter.com/"><FaTwitter className='iconliks' size={26} color='#37b1e2' /></a>
                 </div>
               </div>
             </div>
-
-
           </div>
         </div>
       </div>
-
     </>
   )
 }
