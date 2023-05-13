cdfc94b4c0e57d2e17b79079c98dddf5e04a1db2
Iniy96 <iniyavansivaraj07@gmail.com>
2023-05-13 22:19:39 +0530

    added collapse animation to about ys team content

--- src/pages/whoweare/whoweareteamcontent/WhoweareTeamContent.jsx
+++ src/pages/whoweare/whoweareteamcontent/WhoweareTeamContent.jsx
@@ -1,4 +1,4 @@
-import React,{useEffect} from 'react'
+import React, { useEffect } from 'react'
 
 import { motion, useAnimation } from 'framer-motion'
 import { useInView } from 'react-intersection-observer'
@@ -37,19 +37,25 @@ export const WhoweareTeamContent = () => {
     <>
       <div className='col  mt-5' style={{ padding: "0 25px" }}>
         <div className="d-flex flex-column flex-lg-row gap-4 ">
-          <motion.div className='border d-flex flex-column WhoweareTeamContent' ref={whowearTeameref1} animate={whowearTeameAnimation1}>
+          <motion.div className='border d-flex flex-column WhoweareTeamContent' ref={whowearTeameref1} animate={whowearTeameAnimation1} style={{width:"60%"}}>
             <h5 className='text-primary mb-3 fw-700'>About MCABEE</h5>
             <p>A team that treats technology as a passion!</p>
             <p style={{ textAlign: "justify" }}>MCABEE is a company founded with the aim of making some useful applications that are needed in the daily life of a common man.  It doesn't have to be a completely new invention.  The aim is to create a change in the market by completely replacing an existing product that demands a reinvention.</p>
-            <p style={{ textAlign: "justify" }}>By providing such a product, our desire is to provide a new experience to the customers, completely change the current trend and gain better income through it.  We mostly target subscription-based SaaS products in B2B and B2C segments.  Rather than how many players there are at present, we consider whether it is possible to overcome them if a reinvention is done.  Also, this product should surprise the users.</p>
-            <a href="#" className='btn mt-auto btn-outline-secondary' style={{ width: "fit-Content" }}>MORE</a>
+
+            <p class="collapse" id="collapseExample1" style={{ textAlign: "justify" }}>By providing such a product, our desire is to provide a new experience to the customers, completely change the current trend and gain better income through it.  We mostly target subscription-based SaaS products in B2B and B2C segments.  Rather than how many players there are at present, we consider whether it is possible to overcome them if a reinvention is done.  Also, this product should surprise the users.</p>
+
+            <button className='btn mt-auto btn-outline-secondary' type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample1" aria-expanded="false" aria-controls="collapseExample1" style={{ width: "fit-Content" }}>MORE</button>
           </motion.div>
-          <motion.div className='border d-flex flex-column WhoweareTeamContent' ref={whowearTeameref2} animate={whowearTeameAnimation2}>
+
+          {/* Col 2 */}
+          <motion.div className='border d-flex flex-column WhoweareTeamContent' ref={whowearTeameref2} animate={whowearTeameAnimation2} style={{width:"40%"}}>
             <h5 className='text-primary pb-3  fw-700'>Our Story, Team</h5>
             <p>Our Story</p>
             <p style={{ textAlign: "justify" }}>Originated as a startup company in May 2012, we developed two SaaS products in Healthcare and Architecture segments.  Also, as a web design and development company, we successfully completed web applications for 25+ organizations.</p>
-            <p style={{ textAlign: "justify" }}>Shibin Sebastian, the founder of MCABEE, has 11 years of experience.  An entrepreneur as well as a professional UI/UX design expert, he has been working in the field of web design and development since 2012.</p>
-            <a href="#" className='btn mt-auto  btn-outline-secondary' style={{ width: "fit-Content" }}>MORE</a>
+
+            <p class="collapse" id="collapseExample1" style={{ textAlign: "justify" }}>Shibin Sebastian, the founder of MCABEE, has 11 years of experience.  An entrepreneur as well as a professional UI/UX design expert, he has been working in the field of web design and development since 2012.</p>
+
+            <button className='btn mt-auto  btn-outline-secondary' type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample1" aria-expanded="false" aria-controls="collapseExample1"  style={{ width: "fit-Content" }}>MORE</button>
           </motion.div>
         </div>
       </div>
