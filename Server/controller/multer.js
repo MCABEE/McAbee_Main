428acba37cf06b7b4270a102e8427244e938087d
Iniy96 <iniyavansivaraj07@gmail.com>
2023-05-23 20:43:31 +0530

    code check

--- Server/controller/multer.js
+++ Server/controller/multer.js
@@ -6,9 +6,9 @@ const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         return cb(null, './uploads') // specify the destination folder for uploaded files
     },
-    filename: function (req, file, cb) {
-        return cb(null, `${Date.now()}-${file.originalname}`) // use the original filename for the uploaded file
-    }
+    destination: function (req, file, cb) {
+        return cb(null, './uploads') // specify the destination folder for uploaded files
+    },
 })
 
 const upload = multer({ storage: storage })
