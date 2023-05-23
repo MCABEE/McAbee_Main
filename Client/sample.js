6b79a26939926ba698c232e4b3e9bd01aaec63df
Iniy96 <iniyavansivaraj07@gmail.com>
2023-05-23 18:11:53 +0530

    code deleted

--- Client/sample.js
+++
@@ -1,8 +0,0 @@
-const arr =[1,2,3,4,5,6]
-
-const newarr1 = arr.splice(4,2)
-const newarr2 = arr.slice(2).map((num)=>num)
-
-console.log(arr);
-console.log(newarr1);
-console.log(newarr2);
--- Client/src/components/bottomGradient/BottomGradient.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const BottomGradient = () => {
-  return (
-    <div style={{width:"100%"}}>
-        <img src="src\assets\bottomGradient.png" className='w-100' alt="" />
-    </div>
-  )
-}
